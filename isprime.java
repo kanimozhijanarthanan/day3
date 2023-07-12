8035c858e2279adbc18a72e8a93ab3731278a99c
kanimozhijanarthanan <138613369+kanimozhijanarthanan@users.noreply.github.com>
2023-07-12 14:25:03 +0000

    first commit

---
+++ isprime.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import  java.util.*;
+class UserMainCode
+{
+ public int isPrime(int input1){
+  int count=0;
+ for(int i=2;i<=Math.sqrt(input1);i++)
+{
+ if(input1%i==0)
+     count++;
+}
+if(count==0)
+ return 2;
+ else
+return 1;
+ }
+}
